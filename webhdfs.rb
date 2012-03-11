0d7e1e06f2ab667c695e859cb9eeed0df43f538b
Xing, Wang <zenja.wang@gmail.com>
2012-03-11 18:06:12 +0800

    finish copy_to_local

--- webhdfs.rb
+++ webhdfs.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'json'
 require 'rest_client'
+require 'open-uri'
 
 class WebHDFS
   WEBHDFS_CONTEXT_ROOT="/webhdfs/v1"
@@ -78,6 +79,48 @@ class WebHDFS
   end
 
   def copy_to_local(source_path, target_path)
+    url_path = @url_prefix + WEBHDFS_CONTEXT_ROOT + source_path + '?op=OPEN&user.name=' + @username
+    uri = URI(url_path)
+    # Note: uri.path and uri.request_uri are different, 
+    # uri.request_uri is uri.path plus request params
+    req = Net::HTTP::Get.new(uri.request_uri)
+
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    if (res.class != Net::HTTPTemporaryRedirect) && (res.class != Net::HTTPForbidden) then
+      return make_result(is_successful = false, 
+                       message = "response type is #{res.class.to_s} insdead of Net::HTTPTemporaryRedirect", 
+                       body = {})
+    # if target file is empty, we will get a Net::HTTPForbidden
+    elsif res.class == Net::HTTPForbidden then
+      begin
+        # create an empty file
+        emptyfile = File.new(target_path, 'w')
+        return make_result(is_successful = true)
+      rescue
+        return make_result(is_successful = false, message = "Error when creating empty file #{target_path}")
+      ensure
+        emptyfile.close
+      end
+    end
+
+    redirect_location = res['location']
+
+    # download file
+    begin
+      File.open(target_path, "wb") do |saved_file|
+        # the following "open" is provided by open-uri
+        open(redirect_location) do |read_file|
+          saved_file.write(read_file.read)
+        end
+      end
+      return make_result(is_successful = true, message = "HDFS file #{source_path} downloaded")
+    rescue Exception => e
+      return make_result(is_successful = false, 
+                         message = "Error when downloading file from #{redirect_location}. Msg: #{e.message}")
+    end
   end
 
   def listdir(path)
